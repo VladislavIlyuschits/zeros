c6be03f85f9472e77a7298f765020137d28774dc
Vladislav <ilyuschitsvlad@gmail.com>
2018-03-07 01:24:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+var result = 0;
+var n=1;
+while(number>=1) 
+{
+result+=Math.floor(number/Math.pow(5,n));
+if(number/Math.pow(5,n) < 1) return result;
+n++;
+}
+}
 }
